c7dd689d97e66bfef0bed78258d0b974bd7f91dd
Sarbojit <sarbojitrana47c@gmail.com>
2025-06-26 17:20:20 +0530

    Fixed the local host address which was previously being used

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const express = require('express');
-const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const path = require('path');
+
 const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const notFound = require('./middleware/notFound');
-const path = require('path');
 
+const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const todoRoutes = require('./routes/todo');
 
 app.use(express.json());
+
 app.use(logger);
 
+const todoRoutes = require('./routes/todo');
 app.use('/todos', todoRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = 3000;
-mongoose.connect('mongodb://127.0.0.1:27017/todo_app', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log('✅ Connected to MongoDB');
-    app.listen(PORT, () => {
-        console.log(`🚀 Server running at http://localhost:${PORT}`);
+// Port
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('✅ Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running at http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('❌ MongoDB connection error:', err);
+        process.exit(1);
     });
-})
-.catch((err) => {
-    console.error('❌ MongoDB connection error:', err);
-    process.exit(1); // kill the server if DB doesn't connect
-});
